5d0b8c457e5869360f40cb05f34c8f07497ef219
NguyenDinhThang1204 <2051063572@e.tlu.edu.vn>
2022-03-23 19:54:36 +0700

    request3

--- Request3/main.js
+++ Request3/main.js
@@ -1,4 +1,6 @@
+var j = 18;
 setInterval(handleRealTime = () => {
+    j++
     let date = new Date();
     let hh = String(date.getHours());
     let mm = String(date.getMinutes());
@@ -8,6 +10,24 @@ setInterval(handleRealTime = () => {
     ss = (ss < 10) ? "0" + ss : ss;
     let time = hh + mm + ss;
     for (var i = 0; i < time.length; i++) {
-        document.getElementById("banner" + (i + 1)).src = "BTTH_icons/0" + time[i] + ".gif";
+        document.getElementById("number" + (i + 1)).src = "BTTH_icons/0" + time[i] + ".gif";
     }
+    let dom = document.getElementById("text")
+    if (hh >= 0 && hh < 12) {
+        dom.innerHTML = "CHÀO BUỔI SÁNG";
+    } else if (hh >= 12 && hh < 17) {
+        dom.innerHTML = "CHÀO BUỔI CHIỀU";
+    } else if (hh >= 17 && hh <= 23) {
+        dom.innerHTML = "CHÀO BUỔI TỐI";
+    }
+    if (j % 2 == 0) {
+        dom.classList.add("text_red");
+        dom.classList.remove("text_black");
+    } else {
+        dom.classList.remove("text_red");
+        dom.classList.add("text_black");
+    }
+    dom.style.fontSize = j + 'px';
+    if(j==23)j=18
 }, 1000);
+
